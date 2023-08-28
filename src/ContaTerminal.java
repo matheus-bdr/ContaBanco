d9ea3aea84b63339f0afc0a5d0403c98426d0961
matheus-bdr <mmatheuscruzs@gmail.com>
2023-08-28 16:46:03 -0300

    commit

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("Olá, informe o seu nome: " );
+        String nome = scanner.next();
+
+        System.out.print("Por favor, digite o número da Agência: " );
+        int agencia = scanner.nextInt();
+
+        System.out.print("Por favor, digite o número da sua conta: " );
+        int conta = scanner.nextInt();
+
+        System.out.print("Por favor, digite o seu saldo: " );
+        float saldo = scanner.nextFloat();
+
+        System.out.println("=========É UM PRAZER TE TER CONCOSCO "+nome+"=========");
+        System.out.println("Obrigado por criar uma conta em nosso banco, sua agência é: "+agencia+", conta: "+conta+" e seu saldo: "+saldo+" já está disponível para saque");
     }
 }
